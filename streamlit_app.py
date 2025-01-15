3c27084d5bf6537b0af301e7ff810b90cb668b22
Aldi Fahluzi Muharam <alfahluzi@gmail.com>
2025-01-15 07:31:24 +0000

    update model to DenseNet

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,17 +15,20 @@ st.write("Sistem pendeteksi kematangan kelapa sawit otomatis dapat bermanfaat un
 
 st.header("Model Overview")
 st.image("assets/img/model.png")
-st.write('Hyper parameter yang digunakan adalah: batch size = 32, input size = (128,128), rescale = 1/255, dan epoch = 30')
+st.write('Hyper parameter yang digunakan adalah: batch size = 25, input size = (224,224), rescale = 1/255, dan epoch = 10')
+st.write("Hasil training menghasilkan: accuracy: 0.8821 - loss: 0.2886 - val_accuracy: 0.8889 - val_loss: 0.3107")
 st.image("assets/img/trainvalgraph.png")
-st.write("Hasil ujicoba menghasilkan: Test Loss: 0.5613 & Test Accuracy: 0.7246")
+st.write("Hasil ujicoba menghasilkan: Test Loss: 0.3291 & Test Accuracy: 0.8502")
 st.image("assets/img/confussionmatrix.png")
 
 st.header("Showcase")
 img_file_buffer = st.camera_input("Take a picture")
 uploaded_file = st.file_uploader("Or, Choose a file")
 
-loaded_model = tf.keras.models.load_model('assets/model/model.h5')
-input_shape = (128,128)
+loaded_model = tf.saved_model.load('assets/model')
+inference_function = loaded_model.signatures["serving_default"]
+
+input_shape = (224,224)
 label = ["Belum matang", "Matang", "Terlalu Matang"]
 
 if img_file_buffer is not None or uploaded_file is not None:
@@ -37,23 +40,21 @@ if img_file_buffer is not None or uploaded_file is not None:
     img_array = np.array(img)
 
     image = Image.open(input_file)
-    # Resize img to fit in model input shape
     image = image.resize(input_shape)
-    # Convert the image to a NumPy array
     image_array = np.array(image)
     image_array = image_array[:, :, :3]  # Keep only the first 3 channels (RGB)
-    # Normalize the pixel values
     image_array = image_array / 255.0
-    # Expand dimensions to match the input shape dimension of the model
     input_image = np.expand_dims(image_array, axis=0)
 
-    predictions = loaded_model.predict(input_image).tolist()[0]
-    predicted_label = np.argmax(predictions, axis=-1)
-    
+    # Predict using the model
+    input_tensor = tf.constant(input_image, dtype=tf.float32)
+    predictions = inference_function(inputs=input_tensor)["output_0"].numpy()
+    predicted_label = np.argmax(predictions, axis=-1)[0]  # Extract label index
+
     st.write("Result:")
-    st.write(f"{100*predictions[predicted_label]:.2f}%",label[predicted_label])
+    st.write(f"{100 * predictions[0][predicted_label]:.2f}%", label[predicted_label])
 
     st.write(pd.DataFrame({
         'Label': label,
-        'Result': predictions,
+        'Result': predictions[0],
     }))
